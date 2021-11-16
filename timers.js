3ba053e4b20616f2a4161cd33c5fbad4d41ba8de
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-16 13:40:48 -0800

    reports progress in terminal with percentage

--- timers.js
+++ timers.js
@@ -1,28 +1,26 @@
-/**
- * Timing functions setTimeOut clearTimeOut setInterval and clearInterval
- * work like they do in the browser and are available globally. here we
- * have written a program that will log done to the console after a 3 second delay
- * 
- * improved upon this using setInterval and clearInterval 
- * to show how long we've been waiting every 500 milliseconds (1/2 second)
- */
-
 //declare waitTime to be 3 seconds
-const waitTime = 3000
+const waitTime = 5000
 const waitInterval = 500
 let currentTime = 0
 
-// adds waitInterval to currentTime and logs out how long we've been waiting
+// adds waitInterval to currentTime, calculates percentage, clears terminal line, sets cursor to 0
+// and overwrites the message with the new percentage
 const incrementTime = () => {
   currentTime += waitInterval
-  console.log(`waiting ${currentTime / 1000} seconds`)
+  const percentage = Math.floor((currentTime/waitTime) * 100)
+  process.stdout.clearLine()
+  process.stdout.cursorTo(0)
+  process.stdout.write(`waiting...${percentage}%`)
 }
 //logs that we're setting a 3 second delay
 console.log(`setting a ${waitTime / 1000} second delay`)
 
-// clears interval from running and logs done to the console
+// stops interval from running, clears current terminal line, moves cursor back to zero
+// and overwrites percentages with 'done'
 const timerFinished = () => {
   clearInterval(interval)
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
   console.log('done')
 }
 
@@ -33,3 +31,20 @@ const interval = setInterval(incrementTime, waitInterval)
 //setTimeout takes the function you want to run as the first argument
 // and it takes the amount of time to wait before running it as the second
 setTimeout(timerFinished, waitTime)
+
+/**
+ * Timing functions setTimeOut clearTimeOut setInterval and clearInterval
+ * work like they do in the browser and are available globally. here we
+ * have written a program that will log done to the console after a 5 second delay
+ * 
+ * 
+ * we have the const Interval on line 27 calls the incrementTime func on line 7 every 
+ * waitInterval (500ms). And everytime that is run we add waitTime to currentTime, set a
+ * the percentage using the currentTime vs the waitTime which is the percentage. 
+ * using process standard output, we clear the line, move cursor back to zero and then
+ * overwrite the last percentage with new value.
+ * after the waitTime is complete, the timerFinished func runs online 18, which stops the interval 
+ * from running, process.stdout clears the line and moves the cursor back to zero, logging 'done' in
+ * place of the percentage!
+ * 
+ */
