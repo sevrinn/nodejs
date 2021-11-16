4bd59d222087f6f6dba77628f48ad11d6161a539
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-16 06:18:39 -0800

    wask prompts user for answer to question

---
+++ questions.js
@@ -0,0 +1,24 @@
+/**
+ * standard input/output allows us to communicate with process while its
+ * running
+ * process.stdin
+ * process.stdout is a writable stream that implements a write program that
+ * we can use to send info out of the program. console.log does the same thing
+ * but with process.stdout we have more control over our string
+ */
+
+//declare questions array
+const questions = [
+  "What is your name?",
+  "What would you rather be doing?",
+  "What is your preferred programming language?"
+]
+
+//ask function takes a parameter and if no argument is given
+//it will start at the first index of the array
+const ask = (i=0) => {
+  process.stdout.write(`\n\n\n ${questions[i]}`)
+  process.stdout.write(` > `)
+}
+
+ask()
