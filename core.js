7e0546c91909d979d60466a9e94661b8a16c1c5d
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-16 15:17:52 -0800

    removed deprecated util.log

--- core.js
+++ core.js
@@ -1,7 +1,9 @@
 const path = require('path')
+const util = require('util')
 //if we wanted to create a path to the uploads directory we can join __dirname, 
 //which gets us to our current directory, and then reference the three files within
 const dirUploads = path.join(__dirname, "www", "files", "uploads")
 
 //logs the full path to the uploads "/Users/sarawelker/Documents/Tutorials/nodejs/www/files/uploads"
 console.log(dirUploads)
+
