c0df00b3f3e1883c6e9a9123feac9d508d3e3585
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-17 07:31:49 -0800

    when user types exit, process says goodbye and exits process

--- node-modules/events.js
+++ node-modules/events.js
@@ -1,4 +1,5 @@
 
+const { POINT_CONVERSION_COMPRESSED } = require("constants")
 const events = require("events")
 
 //create instance of event emitter
@@ -12,10 +13,26 @@ emitter.on("customEvent", (message, user) => {
   console.log(`${user}: ${message}`)
 })
 
-//raises events
-//first arg is string name of event, you can name it what you like but it should make sense, duh. lol
-//next two args are the data we want to pass to the handler
-//second ar is the message
-//third arg is the user
-emitter.emit("customEvent", "Hello World", "Computer")
-emitter.emit("customEvent", "Thats pretty cool huh?", "Sev")
+// //raises events
+// //first arg is string name of event, you can name it what you like but it should make sense, duh. lol
+// //next two args are the data we want to pass to the handler
+// //second ar is the message
+// //third arg is the user
+// emitter.emit("customEvent", "Hello World", "Computer")
+// emitter.emit("customEvent", "Thats pretty cool huh?", "Sev")
+
+process.stdin.on("data", data => {
+  const input = data.toString().trim()
+
+  if(input === "exit") {
+    emitter.emit("customEvent", "Goodbye!", "process")
+    process.exit()
+  }
+
+  emitter.emit("customEvent", input, "terminal")
+})
+
+/**
+ * Event emitter give us a way to raise and handle custom events.
+ * It's asyncronous.
+ */
