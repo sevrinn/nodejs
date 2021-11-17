efefa09c3b4c8349de006d0e1b8f703b450d73ca
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-17 11:08:20 -0800

    logs a file to the console syncronously

--- file-sys-basics/readFile.js
+++ file-sys-basics/readFile.js
@@ -1,1 +1,8 @@
 const fs = require("fs")
+
+//syncronous (blocking) readFileSync takes file to read as 1st arg
+//2nd arg is the coding of the file and since its text, we use utf-8
+const text = fs.readFileSync("./assets/Readme.md", "UTF-8")
+
+//logs the text of the file
+console.log(text)
