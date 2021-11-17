7b955194cce9ace523afb2f2df18ed339e519e5e
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-17 08:06:37 -0800

    added more 'on' events to show how you can create as many of these as needed

--- node-modules/questions.js
+++ node-modules/questions.js
@@ -7,12 +7,15 @@ const questions = [
   "What are you going to do with node js?"
 ];
 
-const answerEvents = collectAnswers(questions, answers => {
-  console.log("Thank you for your answers")
-  console.log(answers)
-  process.exit()
-})
+const answerEvents = collectAnswers(questions)
 
 answerEvents.on("answer", answer => 
 console.log(`question answered: ${answer}`)
 )
+
+answerEvents.on("complete", answers => {
+  console.log("Thank you for your answers. ")
+  console.log(answers)
+})
+
+answerEvents.on("complete", () => process.exit())
