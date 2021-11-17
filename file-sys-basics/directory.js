9ca809bf933b631ab18591ed6e697adb9675ed21
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-17 11:59:11 -0800

    checks if a dir already exists before creating it

--- file-sys-basics/directory.js
+++ file-sys-basics/directory.js
@@ -2,10 +2,17 @@
 
 const fs = require("fs")
 
-fs.mkdir("storage-files", err => {
-  if (err) {
-    console.log("err with creating directory")
-    throw err
-  }
-  console.log("Directory created successfully!")
-})
+//to check if a dir already exists
+if (fs.existsSync("storage-files")) {
+  console.log("Already exists")
+} else {
+  //fs.mkdir takes desired name of dir as 1st arg and the callback and the second
+  //only has err since there is no data to pass back
+  fs.mkdir("storage-files", err => {
+    if (err) {
+      console.log("err with creating directory")
+      throw err
+    }
+    console.log("Directory created successfully!")
+  })
+}
