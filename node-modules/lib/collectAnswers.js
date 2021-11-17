86d1d69b16e9c97656914c01c302728a30c41e71
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-17 07:42:24 -0800

    added event raisers

--- node-modules/lib/collectAnswers.js
+++ node-modules/lib/collectAnswers.js
@@ -19,8 +19,11 @@ module.exports = (questions, done = f => f) => {
   const [firstQuestion] = questions
   //create instance of EventEmitter
   const emitter = new EventEmitter()
+
   //runs after first question is asked, taking given answer as arg
   const questionAnswered = answer => {
+    //raises event on answer
+    emitter.emit("answer", answer)
     //pushes answer to answers arr
     answers.push(answer)
     //if  length of answers arr is less than length of questions arr
@@ -28,11 +31,14 @@ module.exports = (questions, done = f => f) => {
       //readline will ask the next question
       rl.question(questions[answers.length], questionAnswered);
     } else {
-      //
+      //raises event when complete
+      emitter.emit("complete", answers)
       done(answers)
     }
   }
 
   rl.question(firstQuestion, questionAnswered)
+
+  return emitter
 }
 
