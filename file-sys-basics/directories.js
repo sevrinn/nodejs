9a7858733aee07a5bf328776103b32b5ce518040
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-17 14:26:27 -0800

    rename dir, create temp dir

---
+++ file-sys-basics/directories.js
@@ -0,0 +1,30 @@
+const fs = require("fs")
+
+//RENAME DIR
+//renames a file, takes 2 args
+  //1 name of dir to change
+  //2 new name 
+fs.renameSync("./storage-files", "./storage")
+
+
+//CREATE TEMP DIR
+//check if a dir already exists
+if (fs.existsSync("temp")) {
+  console.log("Already exists")
+} else {
+  //fs.mkdir takes desired name of dir as 1st arg and the callback and the second
+  //only has err since there is no data to pass back
+  fs.mkdir("temp", err => {
+    if (err) {
+      console.log("err with creating directory")
+      throw err
+    }
+    console.log("Directory created successfully!")
+  })
+}
+
+// //DELETE DIR
+// //waits 4 seconds, deletes temp
+// setTimeout(() => {
+//   fs.rmdir("./")
+// }, 4000)
