78bceee00bb659b426c32b5442af7c4ec549996e
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-17 11:18:36 -0800

    asynchronusly reads targeted file and logs to console

--- file-sys-basics/readFile.js
+++ file-sys-basics/readFile.js
@@ -1,8 +1,16 @@
 const fs = require("fs")
 
-//syncronous (blocking) readFileSync takes file to read as 1st arg
-//2nd arg is the coding of the file and since its text, we use utf-8
-const text = fs.readFileSync("./assets/Readme.md", "UTF-8")
+// //syncronous (blocking) readFileSync takes file to read as 1st arg
+// //2nd arg is the coding of the file and since its text, we use utf-8
+// const text = fs.readFileSync("./assets/Readme.md", "UTF-8")
 
-//logs the text of the file
-console.log(text)
+// readFile asynchronously reads the entirety of a file
+fs.readFile("./assets/Readme.md", "UTF-8", (err, text) => {
+  if(err){
+    console.log("There was an error reading the contents of the file")
+  }
+
+    //logs the text of the file
+    console.log(text)
+})
+
